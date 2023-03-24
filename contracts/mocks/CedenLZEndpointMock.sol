e96da10c03f429ed864168365865995e1aac2c25
Yurii Kabannik <yura_kabannik@icloud.com>
2023-03-24 15:33:37 +0200

    test: Migrate unit tests for CedenMintPass

---
+++ contracts/mocks/CedenLZEndpointMock.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+import "../../layer-zero/contracts/mocks/LZEndpointMock.sol";
+
+contract CedenLZEndpointMock is LZEndpointMock {
+    constructor(uint16 _chainId) LZEndpointMock(_chainId) {}
+}
---
+++ contracts/mocks/CedenMockToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "../../layer-zero/contracts/mocks/MockToken.sol";
+
+contract CedenMockToken is MockToken {
+    constructor(string memory name_, string memory symbol_) MockToken(name_, symbol_) {}
+}
