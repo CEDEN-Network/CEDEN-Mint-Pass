21f0926a2af1c54dc674abdb1bfe52a33cbdfb9b
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2023-03-24 17:28:00 -0700

    Imported solidity examples package. Imported web3 package. Made mock CedenMintPass with extra rawOwnerOf needed for batch unit test.

--- contracts/CedenMintPass.sol
+++ contracts/CedenMintPass.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../layer-zero/contracts/token/onft/ONFT721.sol";
+import "@layerzerolabs/solidity-examples/contracts/token/onft/ONFT721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
