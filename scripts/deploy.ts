37f6f955b616d534b92e37748a810380fb6ef606
Yurii Kabannik <yura_kabannik@icloud.com>
2023-04-05 04:48:44 +0300

    feat: Update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,24 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const CedenMintPassV1 = await ethers.getContractFactory("CedenMintPassV1");
+  const cedenMintPassV1 = await upgrades.deployProxy(CedenMintPassV1, [
+    "CEDEN Mint Pass",
+    "CMP",
+    150000,
+    "0x66A71Dcef29A0fFBDBE3c6a460a3B5BC225Cd675", // lz endpoint
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // usdc
+    6,
+    "0x93E716Bb3F1CC5c37d07AD6CD10c2681447AF880", // fee receiver
+  ],
+    { initializer: 'initialize' },
   );
+  await cedenMintPassV1.deployed();
+
+  console.log("CedenMintPassV1 deployed to:", cedenMintPassV1.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
-  process.exitCode = 1;
+  process.exit(1);
 });
